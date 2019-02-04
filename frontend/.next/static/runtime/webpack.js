8afc1519b5aeb3349889556ef970cefeb7e45af9
unknown <michaelbotes21@gmail.com>
2019-02-04 18:01:01 +0200

    checkpoint at lesson 10, added styl-ed components to header and page, added custom _document page for next to preload css

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ba1d89f649eb9bd8399f";
+/******/ 	var hotCurrentHash = "1b142fb0bee24f9eb373";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
