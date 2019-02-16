04d9f5dccb83d92140c15c8b61c9bf7bdfe98885
unknown <michaelbotes21@gmail.com>
2019-02-16 22:10:40 +0200

    lesson 14 checkpoint, fixed db to reference prisma.graphql, successfully posted to online prisma instance of DB

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,12 +1,26 @@
-const mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
+const Mutations = {
+
+    async createItem(parent, args, ctx, info) {
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info)
+
+        console.log(item);
         
-        //create a new dog
-        const newDog = {name: args.name};
-        global.dogs.push(newDog);
-        return newDog;
+        return item
     }
+
+
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+        
+    //     //create a new dog
+    //     const newDog = {name: args.name};
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // }
 };
 
-module.exports = mutations;
+module.exports = Mutations;
