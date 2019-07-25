e9d19745aa041935f47f9d65f5daf616530e3c92
Michael Botes <michaelb@harambee.co.za>
2019-07-25 11:50:31 +0200

    Lesson 21: single view item added

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4792c2e1b28778c70581";
+/******/ 	var hotCurrentHash = "844ef4636248cc0bea5f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
