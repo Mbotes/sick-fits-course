666ca76b397f2ce1387900706e776dc87cb6d177
Michael Botes <michaelbotes21@gmail.com>
2019-11-29 14:44:52 +0200

    - infra upgrade

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7eed1a9b38a563c310e3";
+/******/ 	var hotCurrentHash = "99a8865b9769d5f97c52";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
